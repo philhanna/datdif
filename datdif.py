d91ef796bdd85d9f738ab67d243726923f9a2c76
Phil Hanna <ph1204@gmail.com>
2022-09-26 17:33:04 -0400

    Removed parse_date

--- datdif.py
+++ datdif.py
@@ -1,8 +1,6 @@
 #! /usr/bin/python3
 import argparse
 
-from datdif import parse_date
-
 parser = argparse.ArgumentParser(description=f"""\
 Calculates and prints the difference between dates.""")
 
@@ -10,5 +8,3 @@ parser.add_argument("start_date", nargs="?", help="starting date")
 parser.add_argument("end_date", nargs="?", help="ending date")
 parser.add_argument("--days", action="store_true", help="show result in days")
 args = parser.parse_args()
-
-date1 = parse_date(args.start_date)
