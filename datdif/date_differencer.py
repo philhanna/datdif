617f007036f4cf482ba2cb9c44ae0121188477d9
Phil Hanna <ph1204@gmail.com>
2022-09-26 17:35:55 -0400

    Added docstrings

--- datdif/date_differencer.py
+++ datdif/date_differencer.py
@@ -2,18 +2,23 @@ from datetime import datetime, date
 
 
 class DateDifferencer:
+    """Calculates the difference between dates"""
 
     @staticmethod
     def date_format():
+        """Returns the date format used by this class"""
         return "%m/%d/%Y"
 
     @staticmethod
     def current_date():
+        """Returns today's date.
+        Specified as a method here so that it can be mocked in unit tests"""
         output = date.today()
         return output
 
     @staticmethod
     def parse_date(dobj):
+        """Parses a date string into a date object"""
         if isinstance(dobj, date):
             return dobj
         if dobj == "today":
