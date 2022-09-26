cbc7e540d038432c48b52cf6561a622e3357eab2
Phil Hanna <ph1204@gmail.com>
2022-09-26 17:26:29 -0400

    Added unit tests for maxdays and leap_year

--- tests/test_maxdays.py
+++ tests/test_maxdays.py
@@ -1,21 +1,23 @@
 from unittest import TestCase
 
-from datdif import is_leap_year
+from datdif import get_max_days
 
 
 class TestMaxDays(TestCase):
+    """Unit tests for get_max_days"""
 
-    def test_2001_is_leap_year(self):
-        self.assertFalse(is_leap_year(2001))
+    def test_get_max_days_2018_2(self):
+        self.assertEqual(28, get_max_days(2018, 2))
 
-    def test_2018_is_leap_year(self):
-        self.assertFalse(is_leap_year(2018))
+    def test_get_max_days_2020_2(self):
+        self.assertEqual(29, get_max_days(2020, 2))
 
-    def test_2000_is_leap_year(self):
-        self.assertTrue(is_leap_year(2000))
+    def test_get_max_days_2000_2(self):
+        self.assertEqual(29, get_max_days(2000, 2))
 
-    def test_1900_is_leap_year(self):
-        self.assertFalse(is_leap_year(1900))
+    def test_get_max_days_2000_4(self):
+        self.assertEqual(30, get_max_days(2000, 4))
 
-    def test_2020_is_leap_year(self):
-        self.assertTrue(is_leap_year(2020))
+    def test_get_max_days_1953_12(self):
+        self.assertEqual(31, get_max_days(1953, 12))
+
