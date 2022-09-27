0cd4fe2d57b06fc85bbd99bbdc35f9cb25cc6d44
Phil Hanna <ph1204@gmail.com>
2022-09-27 12:51:33 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='datdif',
+    version='1.0.0',
+    packages=['tests', 'datdif'],
+    url='https://github.com/philhanna/datdif',
+    license='MIT License',
+    author='saspeh',
+    author_email='',
+    description='Finds difference between dates'
+)
