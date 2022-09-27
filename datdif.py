0a867480fcf91625b0ab35f2d919d542964b5951
Phil Hanna <ph1204@gmail.com>
2022-09-27 13:01:17 -0400

    Reformatted the mainline description

--- datdif.py
+++ datdif.py
@@ -4,8 +4,12 @@ import sys
 
 from datdif import DateDifferencer
 
-parser = argparse.ArgumentParser(description=f"""\
-Calculates and prints the difference between dates.""")
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description=f"""\
+Calculates and prints the difference between dates. Dates must be in the format MM/DD/YYYY.
+The month and day can be either one or two digits. The year must be four digits.
+The string "today" can be entered for either date.""")
 
 parser.add_argument("start_date", help="starting date")
 parser.add_argument("end_date", help="ending date")
