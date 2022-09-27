cd5e24bccef899f2f7402638fa1adb57a59d7f3f
Phil Hanna <ph1204@gmail.com>
2022-09-26 21:14:20 -0400

    Made start and end date require arguments

--- datdif.py
+++ datdif.py
@@ -4,7 +4,8 @@ import argparse
 parser = argparse.ArgumentParser(description=f"""\
 Calculates and prints the difference between dates.""")
 
-parser.add_argument("start_date", nargs="?", help="starting date")
-parser.add_argument("end_date", nargs="?", help="ending date")
+parser.add_argument("start_date", help="starting date")
+parser.add_argument("end_date", help="ending date")
 parser.add_argument("--days", action="store_true", help="show result in days")
 args = parser.parse_args()
+print(f"DEBUG: {args=}")
