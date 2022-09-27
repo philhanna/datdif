f29173917ad9163efcddd3f7afd2a20bbf997d71
Phil Hanna <ph1204@gmail.com>
2022-09-26 21:02:59 -0400

    Updated docstrings

--- datdif/maxdays.py
+++ datdif/maxdays.py
@@ -1,8 +1,6 @@
 def get_max_days(year: int, month: int) -> int:
-    """
-    Returns the maximum number of days in the specified
-    month in the specified year
-    """
+    """Returns the maximum number of days in the specified
+    month in the specified year"""
     if month in [1, 3, 5, 7, 8, 10, 12]:
         return 31
     if month in [4, 6, 9, 11]:
@@ -11,10 +9,8 @@ def get_max_days(year: int, month: int) -> int:
 
 
 def is_leap_year(year: int) -> bool:
-    """
-    Returns True if the specified year is a leap year
-    """
-    if year % 400 == 0:
+    """Returns True if the specified year is a leap year"""
+    if year % 400 == 0:     # e.g., 2000, but not 1900
         return True
     if year % 100 == 0:
         return False
