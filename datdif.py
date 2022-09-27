0c36e04c494ff90e436647fc647db84da6a83c8b
Phil Hanna <ph1204@gmail.com>
2022-09-27 12:12:34 -0400

    Finished mainline (datdif.py)

--- datdif.py
+++ datdif.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 import argparse
 
+from datdif import DateDifferencer
+
 parser = argparse.ArgumentParser(description=f"""\
 Calculates and prints the difference between dates.""")
 
@@ -8,4 +10,6 @@ parser.add_argument("start_date", help="starting date")
 parser.add_argument("end_date", help="ending date")
 parser.add_argument("--days", action="store_true", help="show result in days")
 args = parser.parse_args()
-print(f"DEBUG: {args=}")
+
+dd = DateDifferencer(args.start_date, args.end_date, days=args.days)
+print(dd)
