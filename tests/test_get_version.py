8b664f82ff716da216bc26f6adb02b387a06b52a
Phil Hanna <ph1204@gmail.com>
2022-12-24 14:56:59 -0500

    Converted unit tests to pytest
    Strengthened the regular expression to allow for whitespace

--- tests/test_get_version.py
+++ tests/test_get_version.py
@@ -1,24 +1,19 @@
 import re
-from pathlib import Path
-from unittest import TestCase
 
 from date_difference import get_version
+from tests import project_root_dir
 
 
-class TestGetVersion(TestCase):
-
-    def setUp(self):
-        self.project_root = Path(__file__).parent.parent
-
-    def test_get_version(self):
-        setup_file = self.project_root.joinpath("setup.py")
-        with open(setup_file) as fp:
-            for line in fp:
-                m = re.search("version=(.*),", line)
-                if m:
-                    strversion = m.group(1)
-                    break
-        expected = strversion.strip("\'\"")
-        actual = get_version()
-        if actual is not None:
-            self.assertEqual(expected, actual)
+def test_get_version():
+    setup_file = project_root_dir.joinpath("setup.py")
+    data = setup_file.read_text()
+    strversion = ""
+    for line in data.splitlines():
+        m = re.search(r"version\s*=\s*(.*),", line)
+        if m:
+            strversion = m.group(1)
+            break
+    expected = strversion.strip("\'\"")
+    actual = get_version()
+    if actual is not None:
+        assert actual == expected
