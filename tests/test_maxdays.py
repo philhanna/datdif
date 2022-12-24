009cf54cf79d90ce70eb101f171c5fe4f5753d5d
Phil Hanna <ph1204@gmail.com>
2022-12-24 15:07:39 -0500

    Converted unit test to pytest

--- tests/test_maxdays.py
+++ tests/test_maxdays.py
@@ -1,23 +1,14 @@
-from unittest import TestCase
+import pytest
 
 from date_difference import get_max_days
 
 
-class TestMaxDays(TestCase):
-    """Unit tests for get_max_days"""
-
-    def test_get_max_days_2018_2(self):
-        self.assertEqual(28, get_max_days(2018, 2))
-
-    def test_get_max_days_2020_2(self):
-        self.assertEqual(29, get_max_days(2020, 2))
-
-    def test_get_max_days_2000_2(self):
-        self.assertEqual(29, get_max_days(2000, 2))
-
-    def test_get_max_days_2000_4(self):
-        self.assertEqual(30, get_max_days(2000, 4))
-
-    def test_get_max_days_1953_12(self):
-        self.assertEqual(31, get_max_days(1953, 12))
-
+@pytest.mark.parametrize("year, month, days", [
+    (1953, 12, 31),
+    (2018, 2, 28),
+    (2000, 2, 29),
+    (2000, 4, 30),
+    (2020, 2, 29),
+])
+def test_maxdays(year, month, days):
+    assert get_max_days(year, month) == days
