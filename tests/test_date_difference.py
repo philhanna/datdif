f6b55f0a5290c42e0e6ca4bff81e7f4fed2dcd13
Phil Hanna <ph1204@gmail.com>
2022-12-24 14:51:29 -0500

    Converted unit tests to pytest

--- tests/test_date_difference.py
+++ tests/test_date_difference.py
@@ -5,20 +5,20 @@ import pytest
 from date_difference import DateDifferencer
 
 
-def test_two_days():
-    start_date = date(2020, 1, 1)
-    end_date = date(2020, 1, 3)
-    dd = DateDifferencer(start_date, end_date, days=False)
-    expected = (0, 0, 2)
-    actual = dd.delta
-    assert actual == expected
-
-
-def test_two_days_in_days():
-    start_date = date(2020, 1, 1)
-    end_date = date(2020, 1, 3)
-    dd = DateDifferencer(start_date, end_date, days=True)
-    expected = 2
+@pytest.mark.parametrize("start_date,end_date,expected,in_days", [
+    ("2020-01-01", "2020-01-03", (0, 0, 2), False),  # two days
+    ("2020-01-01", "2020-01-03", 2, True),  # two days (in days)
+    ("1975-03-02", "1976-03-02", (1, 0, 0), False),  # one year
+    ("1975-03-02", "1976-03-02", 366, True),  # one year in days
+    ("1953-12-04", "2022-12-24", (69, 0, 20), False),  # then to now
+    ("1953-12-04", "1963-11-22", (9, 11, 18), False),  # then to JFK
+    ("1922-01-04", "1922-01-04", (0, 0, 0), False),  # zero
+    ("1922-01-04", "1922-01-04", 0, True),  # zero in days
+])
+def test_date_difference(start_date, end_date, expected, in_days):
+    start_date = date.fromisoformat(start_date)
+    end_date = date.fromisoformat(end_date)
+    dd = DateDifferencer(start_date, end_date, days=in_days)
     actual = dd.delta
     assert actual == expected
 
@@ -32,25 +32,6 @@ def test_two_days_with_date_string_in_days():
     assert actual == expected
 
 
-def test_one_year():
-    start_date = "1975-03-02"
-    end_date = "1976-03-02"
-    dd = DateDifferencer(start_date, end_date, days=False)
-    expected = (1, 0, 0)
-    actual = dd.delta
-    assert actual == expected
-    assert str(dd) == "1 year"
-
-
-def test_one_year_in_days():
-    start_date = "1975-03-02"
-    end_date = "1976-03-02"
-    dd = DateDifferencer(start_date, end_date, days=True)
-    expected = 366
-    actual = dd.delta
-    assert actual == expected
-
-
 def test_today_tomorrow_in_days(monkeypatch):
     monkeypatch.setattr('date_difference.DateDifferencer.current_date', lambda: date(2020, 1, 1))
     start_date = "today"
@@ -59,22 +40,6 @@ def test_today_tomorrow_in_days(monkeypatch):
     dd = DateDifferencer(start_date, end_date, days=True)
     actual = dd.delta
     assert actual == expected
-
-
-def test_then_to_now():
-    start_date = "1953-12-04"
-    end_date = "2022-09-26"
-    dd = DateDifferencer(start_date, end_date, days=False)
-    expected = (68, 9, 22)
-    actual = dd.delta
-    assert actual == expected
-
-
-def test_then_to_jfk():
-    start_date = "1953-12-04"
-    end_date = "1963-11-22"
-    dd = DateDifferencer(start_date, end_date, days=False)
-    assert dd.delta == (9, 11, 18)
 
 
 def test_str_one_day():
@@ -107,24 +72,6 @@ def test_today():
     assert actual == expected
 
 
-def test_zero():
-    start_date = "1922-01-04"
-    end_date = start_date
-    dd = DateDifferencer(start_date, end_date, days=False)
-    expected = (0, 0, 0)
-    actual = dd.delta
-    assert actual == expected
-
-
-def test_zero_in_days():
-    start_date = "1922-01-04"
-    end_date = start_date
-    dd = DateDifferencer(start_date, end_date, days=True)
-    expected = 0
-    actual = dd.delta
-    assert actual == expected
-
-
 def test_bad():
     start_date = "1922-01-04"
     end_date = "1901-01-04"
@@ -141,10 +88,3 @@ def test_bad_in_days():
         DateDifferencer(start_date, end_date, days=True)
     expected = "cannot be"
     assert expected in str(ex.value)
-
-
-def test_failing_days():
-    start_date = "2019-02-23"
-    end_date = "today"
-    dd = DateDifferencer(start_date, end_date, days=True)
-    # print(dd)
