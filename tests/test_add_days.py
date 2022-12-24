e206af684d99b5c61f5f9d4f28590db053c5c1d2
Phil Hanna <ph1204@gmail.com>
2022-12-24 14:18:14 -0500

    Parametrized tests

--- tests/test_add_days.py
+++ tests/test_add_days.py
@@ -1,66 +1,23 @@
 from datetime import date
 
+import pytest
+
 from date_difference import DateRoller
 
 
-def test_add_zero_days():
-    start_date = date(2022, 9, 26)
+@pytest.mark.parametrize("indate,add,expected", [
+    ("2022-09-26", 0, "2022-09-26"),    # zero days
+    ("2022-09-26", 1, "2022-09-27"),    # add one day
+    ("2022-09-26", -1, "2022-09-25"),   # subtract one day
+    ("2020-02-25", 5, "2020-03-01"),    # past leap month end
+    ("2021-02-25", 5, "2021-03-02"),    # past regular month end
+    ("1953-12-25", 10, "1954-01-04"),   # past year end
+    ("1953-01-01", -1, "1952-12-31"),   # backwards past year start
+])
+def test_add_days(indate, add, expected):
+    start_date = date.fromisoformat(indate)
     date_roller = DateRoller(start_date)
-    date_roller.add_days(0)
-    expected = start_date
-    actual = date_roller.date
-    assert actual == expected
-
-
-def test_add_1_day():
-    start_date = date(2022, 9, 26)
-    date_roller = DateRoller(start_date)
-    date_roller.add_days(1)
-    expected = date(2022, 9, 27)
-    actual = date_roller.date
-    assert actual == expected
-
-
-def test_subtract_1_day():
-    start_date = date(2022, 9, 26)
-    date_roller = DateRoller(start_date)
-    date_roller.add_days(-1)
-    expected = date(2022, 9, 25)
-    actual = date_roller.date
-    assert actual == expected
-
-
-def test_past_leap_month_end():
-    start_date = date(2020, 2, 25)
-    date_roller = DateRoller(start_date)
-    date_roller.add_days(5)
-    expected = date(2020, 3, 1)
-    actual = date_roller.date
-    assert actual == expected
-
-
-def test_past_regular_month_end():
-    start_date = date(2021, 2, 25)
-    date_roller = DateRoller(start_date)
-    date_roller.add_days(5)
-    expected = date(2021, 3, 2)
-    actual = date_roller.date
-    assert actual == expected
-
-
-def test_past_year_end():
-    start_date = date(1953, 12, 25)
-    date_roller = DateRoller(start_date)
-    date_roller.add_days(10)
-    expected = date(1954, 1, 4)
-    actual = date_roller.date
-    assert actual == expected
-
-
-def test_past_year_start():
-    start_date = date(1953, 1, 1)
-    date_roller = DateRoller(start_date)
-    date_roller.add_days(-1)
-    expected = date(1952, 12, 31)
+    date_roller.add_days(add)
+    expected = date.fromisoformat(expected)
     actual = date_roller.date
     assert actual == expected
