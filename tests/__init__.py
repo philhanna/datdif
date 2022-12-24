0c3c6d467565d784477a02d00af6cb27b1a9003b
Phil Hanna <ph1204@gmail.com>
2022-12-24 15:13:22 -0500

    Removed unused fixtures

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,43 +1,7 @@
-import sys
-from contextlib import contextmanager
 from pathlib import Path
 
 project_root_dir = Path(__file__).parent.parent
 
-
-@contextmanager
-def stdout_redirected(new_stdout):
-    save_stdout = sys.stdout
-    sys.stdout = new_stdout
-    try:
-        yield None
-    finally:
-        sys.stdout = save_stdout
-
-
-@contextmanager
-def stderr_redirected(new_stderr):
-    save_stderr = sys.stderr
-    sys.stderr = new_stderr
-    try:
-        yield None
-    finally:
-        sys.stderr = save_stderr
-
-
-@contextmanager
-def stdin_redirected(new_stdin):
-    save_stdin = sys.stdin
-    sys.stdin = new_stdin
-    try:
-        yield None
-    finally:
-        sys.stdin = save_stdin
-
-
 __all__ = [
     'project_root_dir',
-    'stdout_redirected',
-    'stderr_redirected',
-    'stdin_redirected',
 ]
